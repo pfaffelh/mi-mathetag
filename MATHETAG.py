769f1266e333e9292ee7018c86f47e286db45a09
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-10-24 00:01:51 +0200

    update

--- MATHETAG.py
+++ MATHETAG.py
@@ -8,33 +8,42 @@ from config import *
 st.set_page_config(page_title="Mathetag Einteilung", layout="wide")
 
 # Es liegt eine csv-Datei vor mit Feldern
-#id,Nachname,Vorname,Klassenstufe,Schule,Name der Mathematiklehrkraft,Vormittagskurs Erstwunsch, Vormittagskurs Zweitwunsch, Nachmittagskurs Erstwunsch, Nachmittagskurs Zweitwunsch
+#Dateiname;Vorname;Nachname;Mail;Vormittagskurs1;Vormittagskurs2;Nachmittagskurs1;Nachmittagskurs2;Schule;Stufe;Mathematiklehrkraft
 # Ziel ist es, zwei Spalten zu Ergänzungen: Zuteilung Vormittag, Zuteilung Nachmittag
 
 # In config.py sind die Workshops definiert. Der "name" gibt dabei jeweils den Workshop an
 
 st.header("Einteilung für den Mathetag am 15.11.2024")
 
+if "workshopreihe" not in st.session_state:
+    st.session_state.workshopreihe = workshopreihe
+
+def update_groesse(wr, w):
+    st.session_state.workshopreihe[st.session_state.workshopreihe.index(wr)]['data'][wr['data'].index(w)]["groesse"] = st.session_state[f"{w['name_kurz']}_size"]
+
 st.write("Es werden folgende Workshops angeboten:")
-for wr in workshopreihe:
-    col0, col1 = st.columns([1,4])
+for wr in st.session_state.workshopreihe:
+    col0, col1, col2 = st.columns([1,4,1])
     col0.write(f"{wr['name']}:")
     for w in wr["data"]:
-        col1.write(f"{w['name']}: {w['titel']} (Größe {w['groesse']})")
+        col0, col1, col2, col3 = st.columns([1,4,1,2])
+        col1.write(f"{w['name']}: {w['titel']}")
+        w['groesse'] = col2.number_input("Größe", min_value = 0, value = w['groesse'], on_change = update_groesse, args = (wr, w,), key = f"{w['name_kurz']}_size")
+        wr["groesse"] = sum([int(w['groesse']) for w in wr['data']])
     col1.write(f"**Insgesamt gibt es {wr['groesse']} Plätze.**")
 st.write("### Anmeldungen")
 st.write("Es liegen Anmeldungen in einer csv-Datei in Folgendem Format vor:")
-df = pd.read_csv("anmeldungen.csv", index_col=False)
-st.write(df)
+df = pd.read_csv("anmeldungen.csv", sep=";")
 
 anmeldungen_csv = st.file_uploader("Upload Anmeldungen")
 
 if anmeldungen_csv:
-    df = pd.read_csv(anmeldungen_csv)
+    df = pd.read_csv(anmeldungen_csv, sep = ";", index_col=False)
+    df = df.drop_duplicates(subset='Nachname', keep='last')
     #df = pd.read_csv("anmeldungen.csv", index_col=False)
     col1.write(f"**Insgesamt gibt es {df.shape[0]} Anmeldungen.**")
     st.write("Nun wird die Einteilung vorgenommen.")
-    for wr in workshopreihe:    
+    for wr in st.session_state.workshopreihe:    
         with st.expander(f"Einteilung von {wr["name"]}"):
             if df.shape[1] > wr['groesse']:
                 st.write(f"**Einteilung in {wr['name']} nicht möglich, da es {df.shape[1]} Anmeldungen, aber nur {wr['groesse']} Plätze gibt.**")
@@ -45,7 +54,7 @@ if anmeldungen_csv:
             wuensche = [list(df[df.columns[i]]) for i in wr["wunschspalten"]]
             allewuensche = [item for sublist in wuensche for item in sublist]        
             # Wünsche müssen mit den Namen der Workshops übereinstimmen!
-            w_namen = [w["name"] for w in wr["data"]]
+            w_namen = [w["name_kurz"] for w in wr["data"]]
             #st.write(allewuensche)
             #st.write(w_namen)
             fehler = [w for w in allewuensche if w not in w_namen]
@@ -61,7 +70,7 @@ if anmeldungen_csv:
             # spalten sind die workshops der einzelnen Reihen, jede Spalte ist ein Platz im Workshop
             spalten = []
             for w in wr["data"]:
-                spalten.extend([w['name'] for i in range(w['groesse'])])
+                spalten.extend([w['name_kurz'] for i in range(w['groesse'])])
             # Zunächst gehen wir von maximalen Kosten aus
             kosten = wr["kosten"][-1] + np.zeros((df.shape[0], len(spalten)))
             for i in range(kosten.shape[0]):
@@ -71,7 +80,7 @@ if anmeldungen_csv:
                             kosten[i,j] = float(wr["kosten"][w])
             row_ind, col_ind = linear_sum_assignment(kosten, maximize=False)
 
-            df[f"Einteilung {wr["name"]}"] = [spalten[j] for j in col_ind]
+            df[f"Einteilung {wr["name"]}"] = [workshopname_dict[spalten[j]] for j in col_ind]
             df[f"Workshopname {wr["name"]}"] = [ workshop_dict[spalten[j]] for j in col_ind ]
 
             # Ein wenig Statistik
--- config.py
+++ config.py
@@ -8,23 +8,25 @@ import streamlit as st
 # kosten[1]: falls man den Zweitwunsch bekommt
 # kosten[2]: falls man keinen der beiden Wünschen bekommt
 
-
 workshopreihe = [
     {
         "name" : "Vormittag",
-        "wunschspalten" : [6, 7],
+        "wunschspalten" : [4, 5],
         "kosten" : [0, 2, 5],
         "data" : [{
-            "name" : "Workshop 1",
-            "titel" : "Modulo-Rechnung und Codierungs-Theorie (Dr. Ernst August von Hammerstein)",
-            "groesse" : 30
+                "name_kurz" : "ws1",
+                "name" : "Workshop 1",
+                "titel" : "Modulo-Rechnung und Codierungs-Theorie (Dr. Ernst August von Hammerstein)",
+                "groesse" : 30
             },
             {
+                "name_kurz" : "ws2",
                 "name" : "Workshop 2",
                 "titel" : "Kreisspiegelungen (Prof. Dr. Wolfgang Soergel)",
                 "groesse" : 30
             },
             {
+                "name_kurz" : "ws3",
                 "name" : "Workshop 3",
                 "titel" : "Verkehrssimulation (Prof. Dr. Dietmar Kröner)",
                 "groesse" : 30
@@ -33,19 +35,22 @@ workshopreihe = [
     },
     {
         "name" : "Nachmittag",
-        "wunschspalten" : [8, 9],
+        "wunschspalten" : [6, 7],
         "kosten" : [0, 2, 5],
         "data" : [{
+                "name_kurz" : "ws4",
                 "name" : "Workshop 4",
                 "titel" : "Siteswap – eine mathematische Beschreibung von Jongliermustern (Stud. Maja König)",
                 "groesse" : 30
                 },
             {
+                "name_kurz" : "ws5",
                 "name" : "Workshop 5",
                 "titel" : "Vom Landkartenfärben und Reiseplänen (M. Sc. Mitja Roeder)",
                 "groesse" : 30
             },
             {
+                "name_kurz" : "ws6",
                 "name" : "Workshop 6",
                 "titel" : "Wie groß ist unendlich groß? (M.Sc. Charlotte Bartnick)",
                 "groesse" : 30
@@ -54,11 +59,11 @@ workshopreihe = [
     }
 ]
 
-workshop_dict = { w["name"] : w["titel"] for wr in workshopreihe for w in wr["data"] }
+workshop_dict = { w["name_kurz"] : w["titel"] for wr in workshopreihe for w in wr["data"] }
+workshopname_dict = { w["name_kurz"] : w["name"] for wr in workshopreihe for w in wr["data"] }
+workshopsize_dict = { w["name_kurz"] : w["groesse"] for wr in workshopreihe for w in wr["data"] }
 
 for wr in workshopreihe:
     wr["anzahl_wuensche"] = len(wr["wunschspalten"])
-    wr["groesse"] = sum([int(w['groesse']) for w in wr['data']])
     if len(wr["wunschspalten"]) + 1 != len(wr["kosten"]):
         st.error(f"Konfiguration fehlerhaft. In {wr['name']} ist eine falsche Anzahl von Kosten angegeben. (Muss eins mehr als die Anzahl der Wunschspalten sein.)") 
-    
